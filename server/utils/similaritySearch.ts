fd9fd246fcdbc2ec2fe43a59a71e840a5021ad64
BilalShahid-13 <bcsm-f21-249@superior.edu.pk>
2025-08-07 13:52:06 +0500

    update cors

--- server/utils/similaritySearch.ts
+++ server/utils/similaritySearch.ts
@@ -27,7 +27,8 @@ export async function faqSimilaritySearch(req: Request, query: string, res: Resp
   const results = await vectorStore.similaritySearch(query, 10);
   const context = results.map((doc) => doc.pageContent).join("\n");
 
-  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL!);
+  res.setHeader("Access-Control-Allow-Origin", "https://tongston-ai-assitant.vercel.app");
+  // res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL!);
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
