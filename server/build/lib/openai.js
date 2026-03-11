d2e1be3574640d1df4f4c457af13e997050c5f09
Bilal Shahid <bcsm-f21-249@superior.edu.pk>
2026-03-11 21:32:23 +0500

    switch to gemini openai adapter

--- server/build/lib/openai.js
+++ server/build/lib/openai.js
@@ -3,10 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const openai_1 = __importDefault(require("openai"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const index_mjs_1 = __importDefault(require("openai/index.mjs"));
 dotenv_1.default.config();
-const openai = new openai_1.default({
+const openai = new index_mjs_1.default({
     apiKey: process.env.OPENAI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
 exports.default = openai;
--- server/lib/openai.ts
+++ server/lib/openai.ts
@@ -1,7 +1,9 @@
-import OpenAI from "openai";
 import dotenv from "dotenv";
+import OpenAI from "openai/index.mjs";
 dotenv.config();
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
 export default openai;
